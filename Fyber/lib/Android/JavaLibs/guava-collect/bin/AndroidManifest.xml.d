8e53a5bb1d200c10ece4e7e92b2eaf5d91e7cde4
Robert Segal <rob@getsetgames.com>
2016-09-22 17:39:04 -0400

    Switches guava-collect-base to using guava-collect

---
+++ Fyber/lib/Android/JavaLibs/guava-collect/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/robertsegal/Documents/Unreal Projects/UE4PluginIntegration/Intermediate/Android/APK/JavaLibs/guava-collect-base/bin/AndroidManifest.xml : \
+/Users/robertsegal/Documents/Unreal Projects/UE4PluginIntegration/Intermediate/Android/APK/JavaLibs/guava-collect-base/AndroidManifest.xml \
---
+++ Fyber/lib/Android/JavaLibs/guava-collect/gen/R.java.d
@@ -0,0 +1,1 @@
+ : /Users/robertsegal/Documents/Unreal Projects/UE4PluginIntegration/Intermediate/Android/APK/JavaLibs/guava-collect-base/bin/AndroidManifest.xml \
