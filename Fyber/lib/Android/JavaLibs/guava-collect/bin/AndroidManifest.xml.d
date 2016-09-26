04fca14513ac2a8d2fa1a96f11c2fc142970914e
Robert Segal <rob@getsetgames.com>
2016-09-25 23:16:04 -0400

    Don't need to version bin folder files

--- Fyber/lib/Android/JavaLibs/guava-collect/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/robertsegal/Documents/Unreal Projects/UE4PluginIntegration/Intermediate/Android/APK/JavaLibs/guava-collect-base/bin/AndroidManifest.xml : \
-/Users/robertsegal/Documents/Unreal Projects/UE4PluginIntegration/Intermediate/Android/APK/JavaLibs/guava-collect-base/AndroidManifest.xml \
